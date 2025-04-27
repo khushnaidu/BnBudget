cfabbd1e003ec1bd2019d12585fc4fb2e70ad57b
Ubuntu <ubuntu@ip-172-31-31-141.us-west-1.compute.internal>
2025-04-27 21:04:35 +0000

    Backend APIs added: full upload services, data ingestion to RDS, app structure cleaned

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,16 +2,13 @@
 
 from flask import Blueprint
 
-# Create a Blueprint for route definitions
-# This allows modularizing your app routes and registering them with the app in __init__.py
-# You can later break this into multiple blueprints like auth, api, admin, etc.
+# Create the main blueprint
+main = Blueprint('main', __name__)
 
-main = Blueprint('main', __name__)  # 'main' is the name of this blueprint
 
 @main.route('/')
 def index():
     """
     Default route to verify that the API is working.
-    Returns a simple welcome message.
     """
     return {"message": "Welcome to BnBudget API"}
